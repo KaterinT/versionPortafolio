2a277892d4d151db50a99900c48a34ab3d244541
KaterinTello <u.kate.tello@gmail.com>
2022-03-15 23:37:13 -0500

    fix:message con libreria emailjs

--- front-end-react/src/container/Footer/Footer.js
+++ front-end-react/src/container/Footer/Footer.js
@@ -1,40 +1,64 @@
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 import { useState } from 'react';
 
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
-import { client } from '../../client';
+// import { client } from '../../client';
 import  { SocialMediaButton } from '../../components';
 import './Footer.scss';
 
 const Footer = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
+  // const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  // const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
-  const { username, email, message } = formData;
+ const [enviado, setEnviado] = useState("");
 
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  // const { username, email, message } = formData;
 
-  const handleSubmit = () => {
-    setLoading(true);
+  // const handleChangeInput = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
 
-    const contact = {
-      _type: 'contact',
-      name: formData.username,
-      email: formData.email,
-      message: formData.message,
-    };
+  // const handleSubmit = () => {
+  //   setLoading(true);
 
-    client.create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
-  };
+  //   // const contact = {
+  //   //   _type: 'contact',
+  //   //   name: formData.username,
+  //   //   email: formData.email,
+  //   //   message: formData.message,
+  //   // };
+  // }
+
+  
+
+
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_59bbf3n', 'template_ns87cgo', form.current, '5z1mRB_K2jrqDmT8G')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset()
+  }
+
+  //   client.create(contact)
+  //     .then(() => {
+  //       setLoading(false);
+  //       setIsFormSubmitted(true);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
 
   return (
     <>
@@ -48,16 +72,16 @@ const Footer = () => {
         </div>
         <div className="app__footer-card">
           <img src={images.wtspIcon} alt="phone" />
-          <a href="https://walink.co/bd6b67" target="_blank" className="p-text">+51 972-198-713</a>
+          <a href="https://api.whatssap.com/send?phone=+51972198713" target="_blank" className="p-text" rel="noreferrer">+51 972-198-713</a>
         </div>
       </div>
-      {!isFormSubmitted ? (
+      {/* {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
           <div className="app__flex">
-            <input className="p-text" type="text" placeholder="Nombre" name="username" value={username} onChange={handleChangeInput} />
+            <input className="p-text" type="text" placeholder="Nombre" name="username" value={username} onChange={handleChangeInput} required />
           </div>
           <div className="app__flex">
-            <input className="p-text" type="email" placeholder="Email" name="email" value={email} onChange={handleChangeInput} />
+            <input className="p-text" type="email" placeholder="Email" name="email" value={email} onChange={handleChangeInput} required/>
           </div>
           <div>
             <textarea
@@ -76,7 +100,23 @@ const Footer = () => {
             Thank you for getting in touch!
           </h3>
         </div>
-      )}
+      )} */}
+        <form className="app__footer-form app__flex" ref={form} onSubmit={sendEmail}>
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Nombre" name="name" required />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Email" name="email" required/>
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Mensaje"
+              name="message"
+            />
+          </div>
+          <button type="submit" className="p-text"> Enviar Mensaje</button>
+        </form>
     </>
   );
 };
