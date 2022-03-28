b432515f61d840efcee277ea211d965db6051f9a
KaterinTello <u.kate.tello@gmail.com>
2022-03-28 18:23:28 -0500

    add:new testimonio

--- front-end-react/src/components/NavigationDots.js
+++ front-end-react/src/components/NavigationDots.js
@@ -4,11 +4,12 @@ const NavigationDots = ({active}) => {
   return (
     <div className="app__navigation">
       {['Inicio', 'Sobre mi', 'Proyectos', 'Tecnologías', 'Testimonio', 'Contáctame'].map((item, index) => (
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
         <a
           href={`#${item}`}
           key={item + index}
           className="app__navigation-dot"
-          style={active === item ? { backgroundColor: '#56bb00' } : {}}
+          style={active === item ? { backgroundColor: '#F0A500' } : {}}
         />
       ))}
     </div>
